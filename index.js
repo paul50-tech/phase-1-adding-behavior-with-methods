37fa5e57a12627a5d8f8b4384b25102abc0fadce
Paul Jubilee Owuoth <pauljubilee50@gmail.com>
2024-10-21 11:39:57 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// Your code here
+
+class Cat {
+    constructor(name, breed) {
+      this.name = name;
+      this.breed = breed;
+    }
+  
+    speak() {
+      return `${this.name} says meow!`;
+    }
+  }
+
+  class Dog {
+    constructor(name, breed) {
+      this.name = name;
+      this.breed = breed;
+    }
+  
+    speak() {
+      return `${this.name} says woof!`;
+    }
+  }
+
+  class Bird {
+    constructor(name, breed) {
+      this.name = name;
+      this.breed = breed;
+    }
+  
+    speak() {
+      if (this.breed.toLowerCase() === "parrot" || this.name === "Pablo") {
+        return `It's me! ${this.name}, the parrot!`;
+      } else {
+        return `${this.name} says squawk!`;
+      }
+    }
+  }
+
+// example of usage
+
+  let myCat = new Cat("Whiskers", "Siamese");
+  console.log(myCat.speak());
+
+  let myDog = new Dog("Rex", "Golden Retriever");
+  console.log(myDog.speak());
+  
+  let myParrot = new Bird("Pablo", "parrot");
+  console.log(myParrot.speak());
+  
+  let myOtherBird = new Bird("Mable", "canary");
+  console.log(myOtherBird.speak());
+  
